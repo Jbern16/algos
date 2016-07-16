ddd50615e0fbb45ac1047cc8cd2088c1e45073f1
Jbern16 <jbern16@gmail.com>
2016-07-16 12:03:24 -0600

    add binary search

---
+++ binary_search.rb
@@ -0,0 +1,24 @@
+
+def binary_search(list, item)
+  low = 0
+  high = list.length - 1 
+  while low <= high do
+    mid = (low + high) / 2 
+    guess = list[mid]  
+    if guess == item  
+      return guess 
+    elsif guess > item   
+      high = mid - 1  
+    else
+      low = mid + 1
+    end 
+  end 
+  nil
+end
+
+
+
+my_list = (1..50).to_a
+
+puts binary_search(my_list, 40) #=> 1
+puts binary_search(my_list, -1) #=> None
